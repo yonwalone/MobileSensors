cb012c272e3a38f221fc1a2f3f1c0bd733666877
yonwalone <lukas.zwaller@gmx.de>
2023-05-23 14:37:23 +0200

    Initial Commit

---
+++ MobileSensors/MobileSensorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileSensorsApp.swift
+//  MobileSensors
+//
+//  Created by Lukas Zwaller on 23.05.23.
+//
+
+import SwiftUI
+
+@main
+struct MobileSensorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
